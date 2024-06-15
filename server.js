a518774a797c945621095449682758058e49c86d
unknown <aleks123kos@gmail.com>
2024-06-15 22:36:18 +0600

    updated server.js

--- server.js
+++ server.js
@@ -45,13 +45,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
