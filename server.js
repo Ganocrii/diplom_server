f08021eb9547844f45b995684dc70822885c47ca
unknown <aleks123kos@gmail.com>
2024-06-16 19:26:06 +0600

    updated server and vercel files

--- server.js
+++ server.js
@@ -1,53 +1,37 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path');
-require('dotenv').config();
+const productsRoute = require('./routes/products');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
+
 const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-const uri = process.env.MONGODB_URI;
-
-if (!uri) {
-  console.error('Error: MONGODB_URI is not defined in .env file');
-  process.exit(1);
-}
-
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-let db;
-
-async function connectDB() {
-  try {
-    await client.connect();
-    console.log("Connected successfully to MongoDB");
-    db = client.db('projectdb');
-    app.use('/api/products', require('./routes/products')(db));
-  } catch (err) {
-    console.error('Error connecting to MongoDB:', err.message);
-    setTimeout(connectDB, 5000);  // Попробовать снова подключиться через 5 секунд
-  }
-}
-
-connectDB().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello, World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.use('/api/products', productsRoute);
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB successfully');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err.message);
+  });
+
+module.exports = app;
