45d462a02c1e544db70c846f80f653c12a937c97
unknown <aleks123kos@gmail.com>
2024-06-16 21:28:36 +0600

    7

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const connectMongoDB = require('./connectMongoDB');
 
@@ -6,6 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
+app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB
